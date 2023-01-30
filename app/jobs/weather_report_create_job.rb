217f12bcd327a3c6a7e5634b4520641d173ae302
andrew <kondratev-28@mail.ru>
2023-01-31 06:19:17 +0700

    response_hour fix

--- app/jobs/weather_report_create_job.rb
+++ app/jobs/weather_report_create_job.rb
@@ -2,8 +2,12 @@ class WeatherReportCreateJob < ApplicationJob
   queue_as :default
 
   def perform(report)
-    response_time = Time.new(report["EpochTime"])
-    response_hour = Time.new(response_time.year, response_time.month, response_time.day, response_time.hour, 0, 0)
+    response_time = Time.at(report["EpochTime"])
+    response_hour = if response_time.min >= 30
+                      Time.new(response_time.year, response_time.month, response_time.day, response_time.hour + 1, 0, 0)
+                    else
+                      Time.new(response_time.year, response_time.month, response_time.day, response_time.hour, 0, 0)
+                    end
     WeatherReport.create(
       timestamp: response_hour.to_i,
       temperature: report["Temperature"]["Metric"]["Value"]
