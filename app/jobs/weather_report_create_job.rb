e120fe81b345cb4084a257844ebe7d6341af025e
andrew <kondratev-28@mail.ru>
2023-02-01 21:14:44 +0700

    job style fix

--- app/jobs/weather_report_create_job.rb
+++ app/jobs/weather_report_create_job.rb
@@ -3,11 +3,14 @@ class WeatherReportCreateJob < ApplicationJob
 
   def perform(report)
     response_time = Time.at(report["EpochTime"])
-    response_hour = if response_time.min >= 30
-                      Time.new(response_time.year, response_time.month, response_time.day, response_time.hour + 1, 0, 0)
-                    else
-                      Time.new(response_time.year, response_time.month, response_time.day, response_time.hour, 0, 0)
-                    end
+    response_hour = Time.new(
+      response_time.year,
+      response_time.month,
+      response_time.day,
+      response_time.min >= 30 ? response_time.hour + 1 : response_time.hour,
+      0,
+      0
+    )
     WeatherReport.create(
       timestamp: response_hour.to_i,
       temperature: report["Temperature"]["Metric"]["Value"]
